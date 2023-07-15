57ad33f563345504c771d0c4919a9845e644e385
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2023-07-15 00:25:20 -0300

    refactor: a little principle of single responsibility

--- internal/market/entity/book.go
+++ internal/market/entity/book.go
@@ -79,17 +79,14 @@ func (b *Book) AddTransaction(transaction *Transaction, wg *sync.WaitGroup) {
 	}
 
 	transaction.SellingOrder.Investor.UpdateAssetPosition(transaction.SellingOrder.Asset.ID, -minShares)
-	transaction.SellingOrder.PendingShares -= minShares
+	transaction.AddSellOrderPendingShares(-minShares)
+
 	transaction.BuyingOrder.Investor.UpdateAssetPosition(transaction.BuyingOrder.Asset.ID, minShares)
-	transaction.BuyingOrder.PendingShares -= minShares
+	transaction.AddBuyOrderPendingShares(-minShares)
 
-	transaction.Total = float64(transaction.Shares) * transaction.BuyingOrder.Price
+	transaction.CalculateTotal(transaction.Shares, transaction.BuyingOrder.Price)
+	transaction.CloseBuyOrder()
+	transaction.CloseSellOrder()
 
-	if transaction.BuyingOrder.PendingShares == 0 {
-		transaction.BuyingOrder.Status = "CLOSED"
-	}
-	if transaction.SellingOrder.PendingShares == 0 {
-		transaction.SellingOrder.Status = "CLOSED"
-	}
 	b.Transactions = append(b.Transactions, transaction)
 }
